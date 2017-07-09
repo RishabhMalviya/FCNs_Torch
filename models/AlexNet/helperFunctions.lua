070f6bef809f79cb0a58f6ba395da8b55b84ba7b
RishabhMalviya <RishabhMalviya@users.noreply.github.com>
2017-07-10 03:01:46 +0530

    Correct corner cases for helper function computeOutputDims

--- models/AlexNet/helperFunctions.lua
+++ models/AlexNet/helperFunctions.lua
@@ -13,17 +13,31 @@ function helper.groupedConvolution(inC, outC, W, H, kw, kh, dw, dh, pw, ph, grou
     groupedConv:resize(group)
     net:add(groupedConv)
     
-    net:add(nn.View(group, inC/group, W, H))
-    net:add(nn.SplitTable(1))
+    --net:add(nn.View(group, inC/group, W, H))
+    --net:add(nn.SplitTable(1))
     net:add(nn.CAddTable())
     
     return net
 end
 
-function helper.computeOutputDims(W,H,kw,kh,dw,dh,pw,ph)
-    outW = math.floor((W + 2*pw -kw)/dw + 1)
-    outH = math.floor((H + 2*ph - kh)/dh + 1)
+function helper.computeOutputDims(W,H,kw,kh,dw,dh,pw,ph,ceil)
+    ceil = ceil or nil
     
+    outW = (W + 2*pw - kw)/dw + 1
+    outH = (H + 2*ph - kh)/dh + 1
+
+    if((W + 2*pw - kw)%dw > pw or ceil) then
+        outW = math.ceil(outW)
+    else
+        outW = math.floor(outW)
+    end
+
+    if((H + 2*ph - kh)%dh > ph or ceil) then
+        outH = math.ceil(outH)
+    else
+        outH = math.floor(outH)
+    end
+
     return outW, outH
 end
 
